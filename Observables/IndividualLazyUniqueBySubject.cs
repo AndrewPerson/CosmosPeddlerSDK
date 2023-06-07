d241670399f046dfb3c5f4e59d713bc74a850640
Andrew Pye <professor_smart@hotmail.com>
2023-06-08 07:59:45 +1000

    Removed unused observer code

--- Observables/IndividualLazyUniqueBySubject.cs
+++ Observables/IndividualLazyUniqueBySubject.cs
@@ -2,17 +2,10 @@ using System.Collections.Concurrent;
 
 namespace CosmosPeddler.SDK.Observables;
 
-public class IndividualLazyUniqueBySubject<UniqueByT, ValueT> : IObservable<(UniqueByT, ValueT)> where UniqueByT : notnull
+public class IndividualLazyUniqueBySubject<UniqueByT, ValueT> where UniqueByT : notnull
 {
     private readonly Func<UniqueByT, Task<ValueT>> valueFactory;
     private readonly ConcurrentDictionary<UniqueByT, LazySubject<ValueT>> _subjects = new();
-
-    private readonly ConcurrentDictionary
-    <
-        IObserver<(UniqueByT, ValueT)>,
-        (IObserver<(UniqueByT, ValueT)>, ConcurrentDictionary<UniqueByT, IDisposable>)
-    >
-    observers = new();
 
     public IObservable<ValueT> this[UniqueByT key] => GetOrCreate(key);
 
@@ -23,16 +16,6 @@ public class IndividualLazyUniqueBySubject<UniqueByT, ValueT> : IObservable<(Uni
             key =>
             {
                 var subject = new LazySubject<ValueT>(() => valueFactory(key));
-
-                foreach (var (observer, subscriptions) in observers.Values)
-                {
-                    var subscription = subject.Subscribe(val => observer.OnNext((key, val)));
-                    subscriptions.AddOrUpdate(key, _ => subscription, (_, old) =>
-                    {
-                        old.Dispose();
-                        return subscription;
-                    });
-                }
 
                 return subject;
             },
@@ -52,58 +35,13 @@ public class IndividualLazyUniqueBySubject<UniqueByT, ValueT> : IObservable<(Uni
             {
                 var subject = new LazySubject<ValueT>(() => valueFactory(key));
 
-                foreach (var (observer, subscriptions) in observers.Values)
-                {
-                    var subscription = subject.Subscribe(val => observer.OnNext((key, val)));
-                    subscriptions.AddOrUpdate(key, _ => subscription, (_, old) =>
-                    {
-                        old.Dispose();
-                        return subscription;
-                    });
-                }
-
                 return subject;
             }
         );
     }
 
-    public IDisposable Subscribe(IObserver<(UniqueByT, ValueT)> observer)
-    {
-        var subscriptions = new ConcurrentDictionary<UniqueByT, IDisposable>();
-        observers.TryAdd(observer, (observer, subscriptions));
-
-        foreach (var (key, subject) in _subjects)
-        {
-            var subscription = subject.Subscribe(val => observer.OnNext((key, val)));
-            subscriptions.AddOrUpdate(key, _ => subscription, (_, old) =>
-            {
-                old.Dispose();
-                return subscription;
-            });
-        }
-
-        return new Unsubscriber(this, observer);
-    }
-
     public IndividualLazyUniqueBySubject(Func<UniqueByT, Task<ValueT>> valueFactory)
     {
         this.valueFactory = valueFactory;
-    }
-
-    private class Unsubscriber : IDisposable
-    {
-        private IndividualLazyUniqueBySubject<UniqueByT, ValueT> subject;
-        private IObserver<(UniqueByT, ValueT)> observer;
-
-        public Unsubscriber(IndividualLazyUniqueBySubject<UniqueByT, ValueT> subject, IObserver<(UniqueByT, ValueT)> observer)
-        {
-            this.subject = subject;
-            this.observer = observer;
-        }
-
-        public void Dispose()
-        {
-            subject.observers.Remove(observer, out _);
-        }
     }
 }
